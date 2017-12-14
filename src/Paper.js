1d271a30d76028b9fb1af5d7f3a1243f0805ee04
igarcia <igarcia@nearsoft.com>
2017-12-14 16:32:39 -0600

    Solvable version

--- src/Paper.js
+++ src/Paper.js
@@ -1,27 +1,7 @@
 'use strict';
 
-// Implement here your Magazine logic
+// Implement here your Paper logic
 var Text = require('./Text');
-function Paper(title, year, authors_array, journal){
-
-    // Llamamos al constructor padre, nos aseguramos (utilizando Function#call) que "this" se
-    // ha establecido correctamente durante la llamada
-    Text.call(this, title, year, authors_array);
-
-    this.journal = journal;
-}
-
-// Creamos el objeto Estudiante.prototype que hereda desde Persona.prototype
-Paper.prototype = Object.create(Text.prototype);    // Vea las siguientes notas
-
-// Establecer la propiedad "constructor" para referencias a Estudiante
-Paper.prototype.constructor = Paper;
-
-Paper.prototype.technical_sheet = function () {
-    return "Title: "     + this.title + "\n" +
-           "Year: "      + this.year + "\n" +
-           "Aurhors: "   + this.f_authors() + "\n" +
-           "Journal: "   + this.journal;
-};
+var Paper;
 
 module.exports = Paper;
--- src/Text.js
+++ src/Text.js
@@ -1,20 +1,6 @@
 'use strict';
 
 // Implement here your Text logic
-function Text(title, year, authors_array){
-    this.title = title;
-    this.year = year;
-    this.authors_array = authors_array;
-}
-
-Text.prototype.f_authors = function () {
-    return this.authors_array.join(', ');
-};
-
-Text.prototype.technical_sheet = function () {
-    return "Title: "   + this.title + "\n" +
-    "Year: "    + this.year + "\n" +
-    "Aurhors: " + this.f_authors() + "\n";
-};
+var Text;
 
 module.exports = Text;
