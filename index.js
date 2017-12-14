6f13a734c341c4890dc0dafeebcc21306e3a71b4
igarcia <igarcia@nearsoft.com>
2017-12-14 16:11:28 -0600

    Modeled Library

---
+++ index.js
@@ -0,0 +1,1 @@
+// Main program to show the results
--- src/Library.js
+++ src/Library.js
@@ -1,4 +1,17 @@
 // Implement here your Library logic
-var Library;
+function Library() {
+    this.collection = []
+}
+
+Library.prototype.addText = function (text) {
+    this.collection.push(text)
+};
+
+Library.prototype.showCollection = function () {
+    console.log("%c This is the current collection: ", 'color: #42f44b');
+    this.collection.forEach(function (text) {
+        console.log("$c " + text.technical_sheet, 'color: #f4e241')
+    });
+};
 
 module.exports = Library;
